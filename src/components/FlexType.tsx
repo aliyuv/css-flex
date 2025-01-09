93f92ee0281981942384c765aebdd10b18c05671
aliyuv <1324149139@qq.com>
2025-01-09 19:51:25 +0800

    修复了 当切换状态时不符合预期的动画

--- src/components/FlexType.tsx
+++ src/components/FlexType.tsx
@@ -1,31 +1,38 @@
 import styles from "./FlexType.module.css";
 import {motion} from "motion/react";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function FlexType() {
     const worlds = ['Hello', 'to', 'the', 'World']
     const types = ['block', 'flex']
     const [typesName, setTypeName] = useState<'block' | 'flex'>('block')
+    const elRef = useRef<HTMLUListElement>(null)
+    useEffect(()=>{
+        //组件渲染后执行 执行一次 获取元素节点  id获取元素
+        console.log(elRef.current)
+    },[])
     return (
         <>
-            <div className={styles.container}>
+            <div className={styles.container} >
                 <div className={styles.containerBox}>
-                    <motion.ul style={{display: typesName}} layout="preserve-aspect">
-                        {worlds.map((item, index) => (
-                                <motion.li key={index} layout style={{height: typesName === "flex" ? "300px" : "auto", }}>
-                                    <div className={styles.topline}></div>
-                                    {item}
-                                </motion.li>
-                        ))}
-                    </motion.ul>
+                        <motion.ul style={{display: typesName}}  ref={elRef}>
+                            {worlds.map((item, index) => (
+                                <motion.div key={index} className={styles.libox} layout
+                                            style={{display: typesName === "flex" ? "flex" : "block"}}>
+                                    <motion.li layout>
+                                            <motion.span layout="preserve-aspect">{item}</motion.span>
+                                    </motion.li>
+                                </motion.div>
+                            ))}
+                        </motion.ul>
                 </div>
                 <div className={styles.dis}>
                     <div className={styles.arrt}>display:</div>
                     <div className={styles.type}>
                         {types.map((item, index) => (
-                                <div className={styles.btn} key={index}>
-                                    <motion.button onClick={() => setTypeName(item as 'block' | 'flex')}>
-                                        <motion.div key={index}>{item}</motion.div>
+                            <div className={styles.btn} key={index}>
+                                <motion.button onClick={() => setTypeName(item as 'block' | 'flex')}>
+                                    <motion.div key={index}>{item}</motion.div>
                                     </motion.button>
                                     <motion.div
                                         className={typesName === item ? styles.activeButton : styles.defaultButton}
