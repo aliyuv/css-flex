ed367d957f49dee72177f3c6a21352a314d2350b
aliyuv <1324149139@qq.com>
2025-01-26 20:45:26 +0800

    组件样式基本样式完毕

--- src/components/GridArea/GridArea.tsx
+++ src/components/GridArea/GridArea.tsx
@@ -1,9 +1,53 @@
 import './GridArea.css'
 
 export default function GridArea() {
+  const containerName = ['sidebar', 'header', 'main']
   return (
     <>
-      <div>hell world</div>
+      <div className="ga-contaniner">
+        <div className="ga-contaniner-box">
+          <div className="ga-tabs">
+            <div className="ga-dec">
+              <span>
+                Grid Area
+                <span>:</span>
+              </span>
+            </div>
+            <div className="ga-items">
+              {
+                containerName.map((item, i) => {
+                  return (
+                    <div className="ga-box" key={i}>
+                      <div className="ga-box-dec">{item}</div>
+                      <div className="ga-bottomLine"></div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <div className="ga-area">
+            <div className="ga-grid">
+              <div className="ga-top-line"></div>
+              <div className="ga-bottom-line"></div>
+              <div className="ga-sidebar">
+                <div>sidebar</div>
+              </div>
+              <div className="ga-header">
+                <div>header</div>
+              </div>
+              <div className="ga-main">
+                <div>main</div>
+              </div>
+            </div>
+            <div className="ga-shadow">
+              <div className="ga-shandow-background">
+                <div className="ga-inner-box"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,16 +1,12 @@
 import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox'
 import FlexTypeV from '../components/FlexTypeV/FlexTypeV.tsx'
 import { PaletteTree } from './palette'
-import GridArea from "../components/GridArea/GridArea.tsx";
 
 function ComponentPreviews() {
   return (
     <Previews palette={<PaletteTree />}>
       <ComponentPreview path="/FlexTypeV">
         <FlexTypeV />
-      </ComponentPreview>
-      <ComponentPreview path="/GridArea">
-        <GridArea/>
       </ComponentPreview>
     </Previews>
   )
